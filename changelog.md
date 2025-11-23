bbaee4f62b8fdeb50917d44cbfc3a921e5b86870
Shian <shian.raveneau@yahoo.co.uk>
2025-11-23 13:02:26 +0000

    [v0.02] Added initial Python environment and Raw Data preview script

--- changelog.md
+++ changelog.md
@@ -6,4 +6,10 @@
 - Organized data by platform into data_raw.
 - Committed dataset to GitHub.
 
-
+## [v0.2] - Initial Raw Data Inspection
+- Created Python environment and installed pandas/numpy/matplotlib.
+- Script: 01_preview_raw_data.py
+- Actions:
+    - Created script to inspect raw CSV files.
+    - Verified structure of games, players, prices, purchased_games tables for all platforms.
+    - Identified initial data quality issues for future cleaning (missing values, datatype inconsistencies, etc.).
---
+++ python/01_preview_raw_data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+# Creates file paths (relative to the project folder)
+platforms = ["playstation", "steam", "xbox"]
+tables = ["games", "players", "prices", "purchased_games"]
+base_path = "./data_raw/"
+
+#Loops through each string in the 'platforms' list and assigns it to the temp. variable 'platform'
+#Actions the code below it then reassigns the value of 'platform' to the next string in the list until there are none left 
+for platform in platforms:
+    print(f"\n===== PLATFORM: {platform.upper()} =====") #'f-string' - used for formatting | '\n' - new line
+    # generates a file path - 'f' instructs python to use the actual values stored in the {} in the string
+    #takes the string in variable 'platform' and converts all charactesr to upper case
+
+    for table in tables:
+        file_path = f"{base_path}{platform}/{table}.csv"
+        # the value of the variable 'file_path is printed out because it is in {}
+        print(f"\n--- Loading: {file_path} ---")
+
+        try:
+            # Using pandas (pd) this reads the .csv file and stores the information in a data frame (df)
+            df = pd.read_csv(file_path)
+            print(df.head())  # Shows first rows
+            print(df.info())  # Shows data types
+        except Exception as error_message:
+            print(f"Error loading {file_path}: {error_message}")
+            # if the try fails then this will display the file path and assign the error to the variable 'error_message'
+
+print("\n===== INSPECTION COMPLETE =====")
